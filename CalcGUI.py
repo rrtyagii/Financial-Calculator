634c56819764e176918d81b92161846d60027bd2
rrtyagii <rtyagi1@hawk.iit.edu>
2020-06-10 18:14:13 -0500

    final commit

--- CalcGUI.py
+++ CalcGUI.py
@@ -8,8 +8,8 @@ bo = {'size':(10,1), 'font':('Franklin Gothic Book', 24), 'button_color':('black
 
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
-layout = [  [sg.Text('RT_100_X1', justification='left', size=(50,1), background_color="#272533", text_color='white' )],
-            [ sg.Text('0.000', size=(18,1), justification='right', font=('digital-7 (mono).ttf',8), text_color='red', relief='sunken', key="DISPLAY")],
+layout = [  [sg.Text('RT_100_X1', justification='right', size=(50,1), background_color="#272533", text_color='white' )],
+            [ sg.Text('0.000', size=(100,3), justification='right', font=('digital-7 (mono).ttf',8), text_color='red', relief='sunken', key="DISPLAY")],
             [ sg.Button("C", **bt),sg.Button("CE",**bt), sg.Button('%',**bt), sg.Button("/",**bt) ,sg.Button("PMT",**bt) ],
             [ sg.Button("7", **bw), sg.Button("8", **bw),  sg.Button("9", **bw), sg.Button("*",**bt), sg.Button("FV",**bt) ],
             [ sg.Button("4", **bw), sg.Button("5", **bw),  sg.Button("6", **bw), sg.Button("-",**bt), sg.Button("PV",**bt)  ],
@@ -18,8 +18,12 @@ layout = [  [sg.Text('RT_100_X1', justification='left', size=(50,1), background_
         ]
 
 # Create the Window
-window = sg.Window('Hp-TI180-II', layout=layout, background_color="#272533", size=(550, 600), return_keyboard_events=True)
+window = sg.Window('Hp-TI180-II', layout=layout, background_color="#272533", size=(550, 415), return_keyboard_events=True)
 # Event Loop to process "events" and get the "values" of the inputs
+
+
+var: dict = {'front':[], 'back':[], 'decimal':False, 'x_val':0.0, 'y_val':0.0, 'result':0.0, 'operator':''}
+
 
 def format_number() -> float:
     return float(''.join(var['front']) + '.' + ''.join(var['back'] ))
@@ -41,15 +45,56 @@ def click_number(event:str):
     update_display(format_number())
 
 
+def clear_click():
+    global var
+    var['front'].clear()
+    var['back'].clear()
+    var['decimal'] = False
+
+
+def operator_click(event:str):
+    global var
+    var['operator'] = event
+    try:
+        var['x_val'] = format_number()
+    except:
+        var['x_val'] = var['result']
+    clear_click()
+
+
+def calculate_click():
+    global var
+    var['y_val'] = format_number()
+    try:
+        var['result'] = eval(str(var['x_val']) + var['operator'] + str(var['y_val']) )
+        update_display(var['result'])
+        clear_click()
+    except:
+        update_display('ERROR! DIV/0')
+        clear_click()
+
+
 
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    print(event)
+    if event == sg.WIN_CLOSED or event == 'Cancel' or event is None:
         break
-    print('You entered ', values[0])
-
-    if event in ['0','1','2','3','4','5','6','7','8','9']:
+#    print('You entered ', values[0])
+    if event in ['0','1','2','3','4','5','6','7','8','9'] :
         click_number(event)
+    if event in ['Escape:27','C','CE']: # 'Escape:27 for keyboard control
+        clear_click()
+        update_display(0.0)
+        var['result'] = 0.0
+    if event in ['+','-','*','/']:
+        operator_click(event)
+    if event == '=': #or event is sg.:
+        calculate_click()
+    if event == '.':
+        var['decimal'] = True
+    if event == '%':
+        update_display(var['result'] / 100.0)
 
 
 window.close()
